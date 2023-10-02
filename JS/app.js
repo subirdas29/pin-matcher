e33146391cb1308b5805d86df9a5c3ce5f1f6cc0
subirdas29 <dasjoy104@gmail.com>
2023-10-02 21:07:27 +0600

    pin generator added

--- JS/app.js
+++ JS/app.js
@@ -1,18 +1,28 @@
-function genaratePin (){
-    const pin = getRandomPin();
-    if(pin.length === 4)
+function getPin (){
+    const pin = genaratePin();
+    const pinString = pin + '';
+    if(pinString.length === 4)
     {
         return pin;
     }
     else{
-        return genaratePin();
+       
+        return getPin();
+        
     }
 }
 
 
 
 function genaratePin(){
-    const pincode = Math.round(Math.random()*10000)+'' // number ta empty string diya jog korar karone number ta string hoi gese
+    const pincode = Math.round(Math.random()*10000)// number ta empty string diya jog korar karone number ta string hoi gese
+    
     return pincode;
 }
 
+document.getElementById('gen-btn').addEventListener('click',function(){
+    const code = getPin();
+   
+    const inputField = document.getElementById('input-area');
+    inputField.value = code
+})
